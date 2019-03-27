e0fda90d9af49a18045a8d8a9a53c64cabd6c6c3
Rafael Oliveira Pinheiro <38002838+ROiPinheiro@users.noreply.github.com>
2019-03-27 11:23:50 -0300

    Fale conosco pronto

--- index.php
+++ index.php
@@ -24,13 +24,13 @@
 		.cor_fundo_2{background-color: #ffdd00;}
 		.cor_fundo_3{background-color: #e70c2c;}
 		.cor_fundo_4{background-color: #ffffff;}
-		.cor_fundo_5{background-color: red;}
+		.cor_fundo_5{background-color: #1c1c1d;}
 		/* COR DE LETRA */
 		.cor_letra_1{color: #09a552;}
 		.cor_letra_2{color: #ffdd00;}
 		.cor_letra_3{color: #e70c2c;}
 		.cor_letra_4{color: #ffffff;}
-		.cor_letra_5{color: red;}
+		.cor_letra_5{color: #1c1c1d;}
 	</style>
 </head>
 
