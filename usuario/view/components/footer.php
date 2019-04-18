bee64f48a2b2712a4b5a7e827fc2e4be5b3656a8
Rafael Oliveira Pinheiro <17259227@CFP127ALUNO.LOCAL>
2019-04-18 09:35:23 -0300

    Inicio das dashboard

---
+++ usuario/view/components/footer.php
@@ -0,0 +1,3 @@
+<footer>    
+    DEVERIA TER ALGUMA COISA AQUI '-'
+</footer>
---
+++ usuario/view/components/header.php
@@ -0,0 +1,9 @@
+<header class="header">
+    <i id="nav" class="fas fa-bars" onclick="toggleMenu()"></i>
+
+    <div>
+        <img src="" alt="">
+        <span>Admin</span>
+        <i class="fas fa-sign-out-alt"></i>
+    </div>
+</header>
